bf167fa203f7b73950a5513d2714a3056e881ff2
tyuhl <tyuhl@old-reliable.com>
2018-07-14 15:27:13 -0400

    Added ability to configure check time

--- BatteryMon/BatteryMon.groovy
+++ BatteryMon/BatteryMon.groovy
@@ -15,11 +15,12 @@ definition(
 	author: "Tim Yuhl",
 	description: "Alert if low battery",
 	category: "Convenience",
-	iconUrl: "https://raw.githubusercontent.com/tyuhl/SmartThings/master/assets/BatteryIcon.png,
-	iconX2Url: "https://raw.githubusercontent.com/tyuhl/SmartThings/master/assets/BatteryIcon%402x.png",
+	iconUrl: "https://raw.githubusercontent.com/tyuhl/SmartThings/master/assets/BatteryIcon.png",
+	iconX2Url: "https://raw.githubusercontent.com/tyuhl/SmartThings/master/assets/BatteryIcon%402x.png"
 )
 
 preferences {
+	def defaultTime = Date.parse("HHmm", "0930")
 	section("About") {
 		paragraph "This app will poll selected devices that use a battery and send an alert or SMS when the level reaches a specified threshold."
 		paragraph "You may configure up to four groups with different thresholds."
@@ -31,6 +32,7 @@ preferences {
 			input "sms_${i}", "phone", title: "Send SMS notification to (optional):", required: false
 		}
 	}
+	input "sched_time", "time", title: "Time when check is done:"
 }
 
 def installed() {
@@ -47,7 +49,8 @@ def updated() {
 def initialize() {
 	// Run at 9:15 am every day
 	// Second Minute Hour DayOfMonth Month DayOfWeek Year
-	schedule("0 15 9 * * ?", check_batteries)
+	// schedule("0 15 9 * * ?", check_batteries)
+	schedule(settings.sched_time, check_batteries)
 	// debug setting
 	// runEvery1Minute(check_batteries)
 	check_batteries()
